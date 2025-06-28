d9fef2da7eb93fd0134886a3a3c60ffdf6774f58
simba.wang <simba1986.wang@gmail.com>
2025-06-28 11:18:23 +0800

    refactor run.sh and run_vggt.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 eval "$(conda shell.bash hook)"
 conda activate vggsfm_tmp
-scene_dir=examples_ZED/fire_fighting_car
-out_dir=output_test
+current_dir=$(pwd)
+scene_name="fire_fighting_car"
+scene_dir=$current_dir/examples_ZED/$scene_name
+out_dir=$current_dir/output_test/$scene_name
 # Feedforward prediction only
 # python demo_colmap.py --scene_dir=$scene_dir --conf_thres_value 3
 # python demo_gradio.py --scene_dir=$scene_dir
@@ -9,5 +11,7 @@ out_dir=output_test
 # With bundle adjustment
 # python crop_image.py --image_dir=$scene_dir/images_origin --output_dir=$scene_dir/images
 rm -rf $out_dir
-python demo_colmap.py --scene_dir=$scene_dir --use_ba --max_query_pts 200 --query_frame_num 10 --vis_thresh 0.20 --max_reproj_error 1000 --shared_camera --output_dir $out_dir --use_sfm --use_calibrated_intrinsic
+# python demo_colmap.py --scene_dir=$scene_dir --use_ba --max_query_pts 200 --query_frame_num 10 --vis_thresh 0.20 --max_reproj_error 1000 --shared_camera --output_dir $out_dir --use_sfm --use_calibrated_intrinsic
+python demo_colmap.py --scene_dir=$scene_dir --use_ba --max_query_pts 200 --query_frame_num 1 --vis_thresh 0.20 --max_reproj_error 1000 --shared_camera --output_dir $out_dir --use_sfm --use_calibrated_intrinsic
 
+# cd viewer && python viewer.py --sequence_folder $scene_dir --reconstruction_folder $out_dir/sfm/ --world_coordinate object
--- run_vggt.sh
+++ run_vggt.sh
@@ -1,8 +1,8 @@
-# seq_list="air_gun clamp cooking_shovel cube cup1 cup2 duck fire_fighting_car glass_cup hammer jep_car lufei mouse pitch plane scisors scisors_1 spoon sprayer wrench"
-seq_list="air_gun clamp cooking_shovel cube cup1 cup2 duck glass_cup hammer jep_car lufei mouse pitch plane scisors scisors_1 spoon sprayer wrench"
+seq_list="air_gun clamp cooking_shovel cube cup1 cup2 duck fire_fighting_car glass_cup hammer jep_car lufei mouse pitch plane scisors scisors_1 spoon sprayer wrench"
+# seq_list="cube"
 data_dir="/home/simba/Documents/project/vggt/examples_ZED"
 out_dir="/home/simba/Documents/project/vggt/output"
 
-# python run_vggt.py --execute_list data_process --process_list copy_image --seq_list $seq_list --dataset_dir $data_dir --src_dir /home/simba/Documents/dataset/WonderHOI/ZED/
+# python run_vggt.py --execute_list data_process --process_list copy_data --seq_list $seq_list --dataset_dir $data_dir --src_dir /home/simba/Documents/dataset/WonderHOI/ZED/ --rebuild
 # python run_vggt.py --execute_list data_process --process_list crop_image --seq_list $seq_list --dataset_dir $data_dir 
-python run_vggt.py --execute_list vggt_process --process_list vggt_colmap --seq_list $seq_list --dataset_dir $data_dir --out_dir $out_dir
+python run_vggt.py --execute_list vggt_process --process_list vggt_colmap --seq_list $seq_list --dataset_dir $data_dir --out_dir $out_dir --rebuild
