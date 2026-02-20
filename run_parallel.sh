01e9dfaee0d51cfbc805a4998c0cf76f9d48536e
origin <luwenxuan916@gmail.com>
2026-02-20 15:55:11 +0800

    [NEW] hoi_pipeline_HY_gen, hoi_pipeline_align_SAM3D_with_HY, hoi_pipeline_3D_points_align_with_HY, hoi_pipeline_HY_omni_gen and hoi_pipeline_HY_to_SAM3D in parallel.sh

--- run_parallel.sh
+++ run_parallel.sh
@@ -5,11 +5,12 @@
 export RUN_ON_SERVER=true
 
 declare -A device_sequences=(
+  [0]="BB12 BB13"
   [1]="ABF12 ABF14"
   [2]="GPMF12 GPMF14"
   [3]="MC1 MC4"
-  [4]="MDF12 MDF14 BB12"
-  [5]="ShSu10 ShSu14 BB13"
+  [4]="MDF12 MDF14 "
+  [5]="ShSu10 ShSu14 "
   [6]="SM2 SM4 GSF12"
   [7]="SMu1 SMu40 GSF13"     
 
@@ -24,26 +25,34 @@ for device in "${!device_sequences[@]}"; do
   
   (
 
-    CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
-      --execute_list obj_process \
-      --process_list hoi_pipeline_data_preprocess hoi_pipeline_get_corres \
-      --seq_list $sequences --rebuild 
+    # CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
+    #   --execute_list obj_process \
+    #   --process_list hoi_pipeline_data_preprocess hoi_pipeline_get_corres \
+    #   --seq_list $sequences --rebuild 
 
 
     CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
       --execute_list obj_process \
       --process_list hoi_pipeline_joint_opt \
       --seq_list $sequences --rebuild 
-
-    CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
-      --execute_list obj_process \
-      --process_list hoi_pipeline_joint_opt \
-      --seq_list $sequences --eval 
 
     CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
       --execute_list obj_process \
       --process_list hoi_pipeline_neus_init \
-      --seq_list $sequences --rebuild       
+      --seq_list $sequences --rebuild  
+
+
+
+    CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
+      --execute_list obj_process \
+      --process_list hoi_pipeline_HY_gen hoi_pipeline_align_SAM3D_with_HY hoi_pipeline_3D_points_align_with_HY hoi_pipeline_HY_omni_gen hoi_pipeline_HY_to_SAM3D \
+      --seq_list $sequences --rebuild  
+
+
+    CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
+      --execute_list obj_process \
+      --process_list hoi_pipeline_joint_opt \
+      --seq_list $sequences --eval     
     
     echo "Running fit_hand on CUDA device $device with sequences: $sequences"
     CUDA_VISIBLE_DEVICES=$device python run_wonder_hoi.py \
