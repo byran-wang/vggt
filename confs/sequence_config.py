fe71353aa1c887f6ccf52b44338cc787c25b182b
simba.wang <simba1986.wang@gmail.com>
2026-03-09 18:47:26 +0800

    [ENH] fix vgg_code_dir.

--- confs/sequence_config.py
+++ confs/sequence_config.py
@@ -9,9 +9,7 @@ else:
     home_dir = os.path.expanduser("~")
     conda_dir = f"{home_dir}/miniconda3"
 
-vggt_code_dir = f"{home_dir.rstrip('/')}/Documents/project/vggt"
-if RUN_ON_SERVER:
-    vggt_code_dir = f"{home_dir.rstrip('/')}/Documents/project/vggt_in_the_wild"
+vggt_code_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if dataset == "zed":
     dataset_dir = f"{home_dir}/Documents/dataset/ZED_wenxuan/"
