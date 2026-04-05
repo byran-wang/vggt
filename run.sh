eedd232fd59a3d10c9eeea029cab5cda02f3212a
simba.wang <simba1986.wang@gmail.com>
2026-04-05 08:37:19 +0800

    [ENH] update run.sh

--- run.sh
+++ run.sh
@@ -1,39 +1,46 @@
 eval "$(conda shell.bash hook)"
 conda activate vggsfm_tmp
 
-current_dir=$(pwd)
-# data_dir=$current_dir/dataset_ZED
-# scenes="cup2"
-data_dir=/home/simba/Documents/dataset/BundleSDF/HO3D_v3/train
-scenes="MC1"
-# scenes=$(ls ${data_dir})
-# scenes="hammer"
-# Feedforward prediction only
-# python demo_colmap.py --scene_dir=$scene_dir --conf_thres_value 3
-# python demo_gradio.py --scene_dir=$scene_dir
+# current_dir=$(pwd)
+# # data_dir=$current_dir/dataset_ZED
+# # scenes="cup2"
+# data_dir=/home/simba/Documents/dataset/BundleSDF/HO3D_v3/train
+# scenes="MC1"
+# # scenes=$(ls ${data_dir})
+# # scenes="hammer"
+# # Feedforward prediction only
+# # python demo_colmap.py --scene_dir=$scene_dir --conf_thres_value 3
+# # python demo_gradio.py --scene_dir=$scene_dir
 
-# With bundle adjustment
+# # With bundle adjustment
 
-for scene in ${scenes}; do
-    echo "Processing scene: $scene"
-    scene_dir=$data_dir/$scene
-    out_dir=$current_dir/output/$scene
-    raw_dir="/home/simba/Documents/dataset/WonderHOI/ZED/${scene}/"
-    frame_interval=5
-    # python cp_origin.py --data_path="${raw_dir}/mask_obj" --output_dir="${scene_dir}/mask_obj_origin" --frame_interval=${frame_interval}
-    # python crop_image.py --image_dir=$scene_dir/mask_obj_origin --output_dir=$scene_dir/images --meta_path="${scene_dir}/meta_origin/0000.pkl"
-    # python cp_origin.py --data_path="${raw_dir}/depth_fs" --output_dir="${scene_dir}/depth_fs_origin" --frame_interval=${frame_interval}
-    # python crop_image.py --image_dir=$scene_dir/depth_fs_origin --output_dir=$scene_dir/depth_fs --meta_path="${scene_dir}/meta_origin/0000.pkl"
-    # rm -rf $out_dir
-    rm -rf ${out_dir}/results
-    # python demo_colmap.py --scene_dir=$scene_dir --max_query_pts 200 --query_frame_num 0 --vis_thresh 0.40 --max_reproj_error 3 --shared_camera \
-    #         --output_dir $out_dir  --use_calibrated_intrinsic --max_frame_num 150 --frame_interval 1 --dataset_type ZED
-    python demo_colmap.py --scene_dir=$scene_dir --max_query_pts 200 --query_frame_num 0 --vis_thresh 0.40 --max_reproj_error 3 --shared_camera \
-            --output_dir $out_dir  --use_calibrated_intrinsic --max_frame_num 750  --frame_interval 5 --dataset_type HO3D    
-    # python mvs.py --data_dir ${scene_dir} --out_dir ${out_dir}
-done
+# for scene in ${scenes}; do
+#     echo "Processing scene: $scene"
+#     scene_dir=$data_dir/$scene
+#     out_dir=$current_dir/output/$scene
+#     raw_dir="/home/simba/Documents/dataset/WonderHOI/ZED/${scene}/"
+#     frame_interval=5
+#     # python cp_origin.py --data_path="${raw_dir}/mask_obj" --output_dir="${scene_dir}/mask_obj_origin" --frame_interval=${frame_interval}
+#     # python crop_image.py --image_dir=$scene_dir/mask_obj_origin --output_dir=$scene_dir/images --meta_path="${scene_dir}/meta_origin/0000.pkl"
+#     # python cp_origin.py --data_path="${raw_dir}/depth_fs" --output_dir="${scene_dir}/depth_fs_origin" --frame_interval=${frame_interval}
+#     # python crop_image.py --image_dir=$scene_dir/depth_fs_origin --output_dir=$scene_dir/depth_fs --meta_path="${scene_dir}/meta_origin/0000.pkl"
+#     # rm -rf $out_dir
+#     rm -rf ${out_dir}/results
+#     # python demo_colmap.py --scene_dir=$scene_dir --max_query_pts 200 --query_frame_num 0 --vis_thresh 0.40 --max_reproj_error 3 --shared_camera \
+#     #         --output_dir $out_dir  --use_calibrated_intrinsic --max_frame_num 150 --frame_interval 1 --dataset_type ZED
+#     python demo_colmap.py --scene_dir=$scene_dir --max_query_pts 200 --query_frame_num 0 --vis_thresh 0.40 --max_reproj_error 3 --shared_camera \
+#             --output_dir $out_dir  --use_calibrated_intrinsic --max_frame_num 750  --frame_interval 5 --dataset_type HO3D    
+#     # python mvs.py --data_dir ${scene_dir} --out_dir ${out_dir}
+# done
 # rm -rf $out_dir
 # python demo_colmap.py --scene_dir=$scene_dir --use_ba --max_query_pts 200 --query_frame_num 10 --vis_thresh 0.20 --max_reproj_error 1000 --shared_camera --output_dir $out_dir --use_sfm --use_calibrated_intrinsic
 # python demo_colmap.py --scene_dir=$scene_dir --use_ba --max_query_pts 200 --query_frame_num 1 --vis_thresh 0.20 --max_reproj_error 1000 --shared_camera --output_dir $out_dir --use_sfm --use_calibrated_intrinsic
 
 # cd viewer && python viewer.py --sequence_folder $scene_dir --reconstruction_folder $out_dir/sfm/ --world_coordinate object
+
+# python run_wonder_hoi.py --execute_list obj_process --process_list ho3d_align_SAM3D_mask ho3d_align_SAM3D_pts --seq_list $seq_list
+# python run_wonder_hoi.py --execute_list obj_process --process_list ho3d_obj_SAM3D_filter_3D --seq_list $seq_list
+# python run_wonder_hoi.py --execute_list obj_process --process_list ho3d_align_SAM3D_mask ho3d_align_SAM3D_pts --seq_list $seq_list
+python run_wonder_hoi.py --execute_list obj_process --process_list ho3d_SAM3D_post_process  --seq_list $seq_list --rebuild
+python run_wonder_hoi.py --execute_list obj_process --process_list hoi_pipeline_eval --seq_list $seq_list --rebuild
+python run_wonder_hoi.py --execute_list obj_process --process_list eval_sum --seq_list $seq_list
